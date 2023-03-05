91c6755de1dbd93b9125b049ac2eaf9e5db5d30e
Mauricio <m.alvergue92@gmail.com>
2023-03-04 22:00:28 -0600

    first commit

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+numero1 = 5
+numero2 = -2
+
+def suma(numero1, numero2):
+    total1 = numero1 + numero2
+    return total1
+
+sumatotal = suma(numero1, numero2)
+print("El resultado es ", sumatotal)
